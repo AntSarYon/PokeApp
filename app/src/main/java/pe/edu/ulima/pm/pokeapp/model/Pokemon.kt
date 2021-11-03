ee612035e5d4043bfb0bc34176181a9989a97659
AntSarYon <antoniosaryon155@gmail.com>
2021-11-03 17:48:32 -0500

    DataClass "Pokemon" creado

--- app/src/main/java/pe/edu/ulima/pm/pokeapp/model/Pokemon.kt
+++ app/src/main/java/pe/edu/ulima/pm/pokeapp/model/Pokemon.kt
@@ -1,4 +1,39 @@
 package pe.edu.ulima.pm.pokeapp.model
 
-class Pokemon {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity()
+data class Pokemon(
+
+    // name --> "bulbasaur"
+    @ColumnInfo(name="name")
+    val name : String,
+
+    //stats[0] > base_stat
+    @ColumnInfo(name="hp")
+    val hp : Integer,
+
+    //stats[1] > base_stat
+    @ColumnInfo(name="attack")
+    val attack : Integer,
+
+    //stats[2] > base_stat
+    @ColumnInfo(name="defense")
+    val defense: Integer,
+
+    //stats[3] > base_stat
+    @ColumnInfo(name="special_attack")
+    val special_attack : Integer,
+
+    //stats[4] > base_stat
+    @ColumnInfo(name="special_defense")
+    val special_defense: Integer,
+
+    // sprites > other > official_artwork > front_default
+    // Devuelve este URL a la imagen del pokemon --> https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png
+    @ColumnInfo(name="url")
+    val URL: Integer
+
+)
--- app/src/main/java/pe/edu/ulima/pm/pokeapp/room/PokemonDao.kt
+++ app/src/main/java/pe/edu/ulima/pm/pokeapp/room/PokemonDao.kt
@@ -1,4 +1,5 @@
 package pe.edu.ulima.pm.pokeapp.room
 
 interface PokemonDao {
+
 }
