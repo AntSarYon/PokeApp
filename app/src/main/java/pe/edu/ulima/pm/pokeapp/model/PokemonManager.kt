dc8acc7e039a6d1fdf3d2b6a352c0b16fc4e24fa
AntSarYon <antoniosaryon155@gmail.com>
2021-11-15 19:52:07 -0500

    dbFirebase Creado en PKManager

--- app/src/main/java/pe/edu/ulima/pm/pokeapp/model/PokemonManager.kt
+++ app/src/main/java/pe/edu/ulima/pm/pokeapp/model/PokemonManager.kt
@@ -3,6 +3,8 @@ package pe.edu.ulima.pm.pokeapp.model
 import android.content.Context
 import android.util.Log
 import androidx.room.Room
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
 import pe.edu.ulima.pm.pokeapp.room.PKAppDatabase
@@ -12,6 +14,9 @@ class PokemonManager(context: Context) {
     val pokeApi = PokeApiClient()
 
     val db = Room.databaseBuilder(context, PKAppDatabase::class.java, "db_pokemon").allowMainThreadQueries().build()
+
+    //private val dbFirebase = Firebase.firestore
+
     //val API_URL = "https://pokeapi.co/api/v2/"
 
     fun getPokemon(callbackOK : (List<Pokemon>) -> Unit , callbackError : (String) ->Unit) {
